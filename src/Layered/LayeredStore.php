61fc40228af5f1af23b2b37f7c93bee1a8d39d78
Cornel Cruceru <amne@amne.me>
2024-04-24 23:04:53 +0300

    WIP: layered store in alpha. many basic tests are failing

--- src/Layered/LayeredStore.php
+++ src/Layered/LayeredStore.php
@@ -7,7 +7,7 @@ namespace MatthiasMullie\Scrapbook\Layered;
 use MatthiasMullie\Scrapbook\KeyValueStore;
 
 /**
- * This class will server as a layered cache adapter that fallbacks to lower
+ * This class will serve as a layered cache adapter that fallbacks to lower
  * key-value stores to fetch the value. Unlike buffered store which is by
  * design "layering" only memory with an additional kv-store, this store
  * can layer an arbitrary number of kv-stores. Arbitrary but use common sense.
@@ -29,23 +29,38 @@ class LayeredStore implements KeyValueStore
     protected array $adapters = [];
 
     /**
-     * @var BufferedStore[]
+     * Array of CAS tokens for each adapter
+     *
+     */
+    protected array $adapterTokens = [];
+
+    /**
+     * @var LayeredStore[]
      */
     private array $collections = [];
+
+    /**
+     * For each valid adapter you can configure a max lifetime
+     *
+     * @var int[]
+     */
+    private array $maxLifetimes = [];
+ 
 
 
 
     /**
      * @param KeyValueStore[] $adapters
+     * @param int[] $maxLifetimes
      */
-    public function __construct(array $adapters)
+    public function __construct(array $adapters, array $maxLifetimes = [])
     {
-        $this->adapters = array_filter(
-            $adapters,
-            function ($a) {
-                return $a instanceof KeyValueStore;
+        foreach ($adapters as $i => $adapter) {
+            if ($adapter instanceof KeyValueStore) {
+                $this->adapters[] = $adapter;
+                $this->maxLifetimes[] = $maxLifetimes[$i] ?? PHP_INT_MAX;
             }
-        );
+        }
     }
 
     /**
@@ -58,16 +73,20 @@ class LayeredStore implements KeyValueStore
     {
         // Start with the first adapter
         $adapterIndex = 0;
-        while ($adapterIndex < count($this->adapters)) {
-            // leave cas token out because it is not usable with layered cache
-            $value = $this->adapters[$adapterIndex]->get($key);
+        $token = uniqid('', false);
+        $this->adapterTokens[$token] = [];
+        $value = false;
+        while (false === $value && $adapterIndex < count($this->adapters)) {
+            $value = $this->adapters[$adapterIndex]->get($key, $adapterToken);
             // if this adapter doesn't have the key go to the next adapter
-            if (false === $value) {
-                $adapterIndex++;
-                continue;
-            }
+            $adapterIndex++;
         }
 
+        if (false === $value) {
+            return false;
+        }
+
+        $this->adapterTokens[$adapterIndex][$token] = $adapterToken;
         // write the missing value in the previous adapters
         while ($adapterIndex) {
             $adapterIndex--;
@@ -78,8 +97,10 @@ class LayeredStore implements KeyValueStore
     }
 
     /**
-     * In addition to all writes being stored to $local, we'll also
-     * keep get() values around ;).
+     * Get multiple keys at once
+     *
+     * We could of course "foreach -> this->get" this but good storages
+     * actually have a batch get capability (getMultiple in PSR16)
      *
      * {@inheritdoc}
      */
@@ -87,97 +108,143 @@ class LayeredStore implements KeyValueStore
     {
         $adapterIndex = 0;
         $adapterMissing = [];
-        while ($adapterIndex < count($this->adapters)) {
-            $values = $this->adapters[$adapterIndex]->getMulti($keys);
-            $missing = array_diff($keys, $values);
-            if (!empty($missing)) {
-                // todo : keep track of resolved vs missing values
+        $adapterTokens = [];
+        $adapterValues = [];
+        $values = [];
+        $missing = $keys;
+        // stop when we've found all the values or we're out of adapters
+        while (!empty($missing) && $adapterIndex < count($this->adapters)) {
+            $adapterValues[$adapterIndex] = $this->adapters[$adapterIndex]->getMulti($missing);
+            // keep track of missing values in each adapter
+            $missing = $adapterMissing[$adapterIndex] = array_diff($missing, $adapterValues[$adapterIndex]);
+            $adapterIndex++;
+        }
+
+        // walk back and update missing values
+        if (count($missing) != count($keys)) {
+            while ($adapterIndex > 0) {
+                $adapterIndex--;
+                $values = array_merge($values, $adapterValues[$adapterIndex+1]);
+                // write the values found in the next adapter
+                // because those were the ones we didn't find in this one
+                // RC: between reads values could have expired in the next adapter
+                // which means we might not have all the missing values
+                $this->adapters[$adapterIndex]->setMulti($values);
             }
         }
 
-        if (!empty($missing)) {
-            $this->local->setMulti($missing);
-        }
+        $tokens = array_walk($values, 'serialize');
 
         return $values;
     }
 
+    private function _getAdapterExpire(int $adapterIndex, int $expire = 0)
+    {
+        return min($expire, $this->maxLifetimes[$adapterIndex]);
+    }
+
     public function set(string $key, mixed $value, int $expire = 0): bool
     {
-        $result = $this->transaction->set($key, $value, $expire);
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($result && $adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->set($key, $value, $this->_getAdapterExpire($adapterIndex, $expire));
+        }
+
+        if (!$result) {
+            // best effort rollback
+            while ($adapterIndex < count($this->adapters)) {
+                $this->adapters[$adapterIndex]->delete($key);
+            }
+        }
 
         return $result;
     }
 
     public function setMulti(array $items, int $expire = 0): array
     {
-        $result = $this->transaction->setMulti($items, $expire);
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($result && $adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->setMulti($items, $this->_getAdapterExpire($adapterIndex, $expire));
+        }
+
+        if (!$result) {
+            // best effort rollback
+            while ($adapterIndex < count($this->adapters)) {
+                $this->adapters[$adapterIndex]->deleteMulti(array_keys($items));
+            }
+        }
 
         return $result;
     }
 
     public function delete(string $key): bool
     {
-        $result = $this->transaction->delete($key);
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->delete($key, $value, $this->_getAdapterExpire($adapterIndex, $expire));
+        }
 
         return $result;
     }
 
     public function deleteMulti(array $keys): array
     {
-        $result = $this->transaction->deleteMulti($keys);
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->deleteMulti($keys, $this->_getAdapterExpire($adapterIndex, $expire));
+        }
 
         return $result;
     }
 
     public function add(string $key, mixed $value, int $expire = 0): bool
     {
-        $result = $this->transaction->add($key, $value, $expire);
-        $this->transaction->commit();
-
-        return $result;
+        throw new \Exception("method not implemented " . __FUNCTION__ , -1);
     }
 
     public function replace(string $key, mixed $value, int $expire = 0): bool
     {
-        $result = $this->transaction->replace($key, $value, $expire);
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->replace($key, $value, $this->_getAdapterExpire($adapterIndex, $expire));
+        }
 
         return $result;
     }
 
     public function cas(mixed $token, string $key, mixed $value, int $expire = 0): bool
     {
-        $result = $this->transaction->cas($token, $key, $value, $expire);
-        $this->transaction->commit();
-
-        return $result;
+        throw new \Exception("method not implemented " . __FUNCTION__ , -1);
     }
 
     public function increment(string $key, int $offset = 1, int $initial = 0, int $expire = 0): int|false
     {
-        $result = $this->transaction->increment($key, $offset, $initial, $expire);
-        $this->transaction->commit();
-
-        return $result;
+        throw new \Exception("method not implemented " . __FUNCTION__ , -1);
     }
 
     public function decrement(string $key, int $offset = 1, int $initial = 0, int $expire = 0): int|false
     {
-        $result = $this->transaction->decrement($key, $offset, $initial, $expire);
-        $this->transaction->commit();
-
-        return $result;
+        throw new \Exception("method not implemented " . __FUNCTION__ , -1);
     }
 
     public function touch(string $key, int $expire): bool
     {
-        $result = $this->transaction->touch($key, $expire);
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->replace($key, $this->_getAdapterExpire($adapterIndex, $expire));
+        }
 
         return $result;
     }
@@ -188,17 +255,27 @@ class LayeredStore implements KeyValueStore
             $collection->flush();
         }
 
-        $result = $this->transaction->flush();
-        $this->transaction->commit();
+        $adapterIndex = count($this->adapters);
+        $result = true;
+        while ($adapterIndex > 0) {
+            $adapterIndex--;
+            $result = $result && $this->adapters[$adapterIndex]->flush();
+        }
 
         return $result;
     }
 
     public function getCollection(string $name): KeyValueStore
     {
+        $adapterCollections = [];
         if (!isset($this->collections[$name])) {
-            $collection = $this->transaction->getCollection($name);
-            $this->collections[$name] = new static($collection);
+            $adapterIndex = 0; 
+            while ($adapterIndex < count($this->adapters)) {
+                $adapterIndex--;
+                $adapterCollections[$adapterIndex] = $this->adapters[$adapterIndex]->getCollection($name);
+            }
+
+            $this->collections[$name] = new static($adapterCollections, $this->maxLifetimes);
         }
 
         return $this->collections[$name];

5c3f448db07e2217c05bfed9594dfc3d92f26eef
cornel cruceru <amne@amne.me>
2024-05-11 00:58:53 +0300

    LayeredStore: reverted packed approach to keep values untouched; now keeping meta in separate key; time to battle test this

--- tests/Layered/AbstractLayeredStoreTestCase.php
+++ tests/Layered/AbstractLayeredStoreTestCase.php
@@ -38,19 +38,31 @@ abstract class AbstractLayeredStoreTestCase extends AbstractKeyValueStoreTestCas
         // in all cache layers
         array_walk(
             $this->subjectKeyValueStores,
-            fn($kvStore) => $this->assertEquals('value', $kvStore->get('key')[0])
+            fn($kvStore) => $this->assertEquals('value', $kvStore->get('key'))
         );
     }
 
     public function testLayeredGetMultiSyncBack(): void
     {
-        $this->subjectKeyValueStores[1]->set('key_1', ['value_1', 0]);
-        $this->subjectKeyValueStores[2]->set('key_2', ['value_2', 0]);
+        $this->subjectKeyValueStores[1]->set('key_1', 'value_1');
+        $this->subjectKeyValueStores[2]->set('key_2', 'value_2');
 
         $this->testKeyValueStore->getMulti(['key_1', 'key_2']);
 
         // test getMulti brings both keys to kvStore[0]
-        $this->assertEquals('value_1', $this->subjectKeyValueStores[0]->get('key_1')[0]);
-        $this->assertEquals('value_2', $this->subjectKeyValueStores[0]->get('key_2')[0]);
+        $this->assertEquals('value_1', $this->subjectKeyValueStores[0]->get('key_1'));
+        $this->assertEquals('value_2', $this->subjectKeyValueStores[0]->get('key_2'));
+    }
+
+    public function testFirstLayerExpires(): void
+    {
+        $this->testKeyValueStore->set('k1', 'v1');
+        $this->assertEquals('v1', $this->subjectKeyValueStores[0]->get('k1'));
+        sleep(3);
+        $this->assertEquals(false, $this->subjectKeyValueStores[0]->get('k1'));
+
+        // refresh it
+        $this->testKeyValueStore->get('k1');
+        $this->assertEquals('v1', $this->subjectKeyValueStores[0]->get('k1'));
     }
 }
